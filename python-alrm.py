a68073889c254a01a9d02dd08c588733ce8c17f8
Hrushikesh <hrishakhade@gmail.com>
2021-02-21 01:13:37 +0530

    Audio File Added

--- python-alrm.py
+++ python-alrm.py
@@ -1,1 +1,16 @@
-print("Alarm")
+
+from playsound import playsound
+import winsound, time
+
+number_of_seconds= int(input("Enter the number of seconds : "))
+
+type_music=int(input("1.System Sound 2.Bella Ciao"))
+
+time.sleep(number_of_seconds);
+
+if type_music==1:
+	for j in range(10):
+		winsound.MessageBeep(-1);
+		time.sleep(1)
+else:
+	playsound('audio.mp3')
